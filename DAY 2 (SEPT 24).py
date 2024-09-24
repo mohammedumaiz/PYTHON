a690c010cdc4fd0c36f5c7d388a4b2d22444a63a
MOHAMMED UMAIZ <umaizmohammed100@gmail.com>
2024-09-24 20:24:58 +0530

    Add files via upload

---
+++ DAY 2 (SEPT 24).py
@@ -0,0 +1,17 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> print (math.sqrt(25))
+5.0
+>>> print (math.sqrt(625))
+25.0
+>>> import cmath
+>>> print (cmath.sqrt(-625))
+25j
+>>> print (math.sqrt(144))
+12.0
+>>> print (cmath.sqrt(-169))
+13j
+>>> import random
+>>> print (random.randint(5000,500000))
+224413
